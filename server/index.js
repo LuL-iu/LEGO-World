4ec0f3bc295865b164027c1eefce31854ee56a21
Yixuan Chen <chenyc@seas.upenn.edu>
2020-12-01 11:03:50 -0500

    callie

--- server/index.js
+++ server/index.js
@@ -18,18 +18,17 @@ app.use(bodyParser.urlencoded({extended: false}));
 // The route localhost:8081/genres is registered to the function
 // routes.getAllGenres, specified in routes.js.
 
-app.get('/top-level-themes', routes.getTopLevelThemes);
+
 app.get('/minifig-actor/:theme', routes.getMinifigActors);
 app.get('/sets/:themeID', routes.getSets);
-app.get('/product/:set_num', routes.getSelectSet);
 app.get('/product/parts/:set_num', routes.getAllParts);
 app.get('/product/reviews/:set_num', routes.getProductReview);
 app.get('/product/similarSet/:set_num', routes.getSimilarSet);
-app.get('/minifig/:fig_num', routes.getMinifigById);
+app.get('/minifig/:fig_num', routes.getMinifigById)
 // app.get('/minifig/all', routes.getMinifigs)
-app.get('/minifig/actor/:fig_num', routes.getActorByFigNum)
-app.get('/search/set/:text', routes.getSearchSet);
-app.get('/search/minifig/:text', routes.getSearchMinifig);
+app.get('/minifig/actor/:fig_num', routes.getActorByFigNum);
+app.get('/minifig/set/:fig_num', routes.querySet)
+app.get('/sets/relative/:set_num', routes.queryRelative)
 
 
 
