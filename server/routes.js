7256c3f4e7b94bd6896bc8a8bc84c8e35ff246c2
DanSangiamo <dan@DanSangiamos-MacBook-Pro.local>
2020-12-14 14:53:39 -0600

    added optimized query

--- server/routes.js
+++ server/routes.js
@@ -11,27 +11,25 @@ function getMinifigActors(req, res) {
   var query = `
     WITH sets_of_interest AS (
       SELECT S.set_num AS set_num
-      FROM theme T JOIN sets S ON S.theme_id = T.id 
+      FROM (SELECT set_num, theme_id FROM sets) S JOIN theme T ON S.theme_id = T.id
       WHERE T.name = '${req.params.theme}'
     ),
-    inventories_to_check AS (
-      SELECT DISTINCT id AS inventory_id 
-      FROM sets_of_interest JOIN inventory ON inventory.set_num = sets_of_interest.set_num
-      UNION
-      SELECT DISTINCT inventory_id
-      FROM  sets_of_interest JOIN inventory_set ON sets_of_interest.set_num = inventory_set.set_num 
-    ),
     minifigs_to_check AS (
-      SELECT *
-      FROM minifig
-      WHERE fig_num IN (
-        SELECT IM.fig_num AS fig_num
-        FROM inventories_to_check ITC JOIN inventory_minifig IM ON ITC.inventory_id = IM.inventory_id
-      )
+        SELECT name AS minifig_name, fig_num, image_url
+        FROM minifig WHERE fig_num IN (
+            SELECT fig_num
+            FROM inventory_minifig
+            WHERE inventory_id IN (
+                SELECT id AS inventory_id 
+                FROM sets_of_interest NATURAL JOIN (SELECT set_num, id FROM inventory) I
+                UNION
+                SELECT inventory_id
+                FROM sets_of_interest NATURAL JOIN (SELECT set_num, inventory_id FROM inventory_set) I
+            )
+        ) AND image_url IS NOT NULL
     )
-    SELECT actor.name AS actor_name, minifig.name AS minifig_name, image_url
-    FROM actor JOIN (inventories_to_check NATURAL JOIN inventory_minifig NATURAL JOIN minifig NATURAL JOIN actor_minifig_mapping) ON actor.id = actor_minifig_mapping.actor_id
-    WHERE image_url IS NOT NULL
+    SELECT actor.name AS actor_name, minifig_name, image_url 
+    FROM actor JOIN (minifigs_to_check NATURAL JOIN actor_minifig_mapping) ON actor.id = actor_minifig_mapping.actor_id
     ORDER BY RAND();
   `;
 
